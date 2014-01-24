f4d15a0ab56659b3e9cb59e442ad63e0b3b5e3c9
Haibo Xi <haibbo@gmail.com>
2014-01-23 23:28:22 -0500

    Calculate speed every 0.2s for a average speed
    Discard the fastest 10% and the slowest 20% speed to get a average speed

--- main.c
+++ main.c
@@ -24,8 +24,11 @@
 #define MAX_IPADDRESS_STRLEN 48
 #define MAX_CLOSEST_SERVER_NUM 5
 
+#define RECORED_EVERY_SEC    0.2
+#define PRINT_RECORED_NUM    2
+
 #define PI                      3.1415926
-#define EARTH_RADIUS            6378.137    
+#define EARTH_RADIUS            6378.137
 
 #define OK  0
 #define NOK 1
@@ -39,6 +42,8 @@ struct thread_para
     char         url[URL_LENGTH_MAX + 32];
     double       result;
     long         upload_size;
+    double       now;
+    pthread_mutex_t lock;
 };
 
 struct web_buffer
@@ -71,9 +76,7 @@ struct server_info servers[MAX_CLOSEST_SERVER_NUM];
 
 static int show_usage(char* argv[])
 {
-    printf("%s:\n\t\t -p number of threads\n" \
-                "\t\t -l list all valid server\n" \
-                "\t\t -s specify mini ookla server url\n", argv[0]);
+    printf("%s:\n\t\t -p number of threads\n"                 "\t\t -l list all valid server\n"                 "\t\t -s specify mini ookla server url\n", argv[0]);
     exit(0);
 }
 
@@ -84,8 +87,16 @@ static int calc_past_time(struct timeval* start, struct timeval* end)
 
 static size_t write_data(void* ptr, size_t size, size_t nmemb, void *stream)
 {
+    struct thread_para *p_thread = (struct thread_para*)stream;
+    if (p_thread) {
+        //p_thread->data++;
+        pthread_mutex_lock(&p_thread->lock);
+        p_thread->now += size * nmemb;
+        pthread_mutex_unlock(&p_thread->lock);
+    }
     return size * nmemb;
 }
+
 
 #if 1
 void *myrealloc(void *ptr, size_t size)
@@ -97,13 +108,13 @@ void *myrealloc(void *ptr, size_t size)
    else
      return malloc(size);
  }
- 
+
  size_t
  write_web_buf(void *ptr, size_t size, size_t nmemb, void *data)
  {
    size_t realsize = size * nmemb;
    struct web_buffer *mem = (struct web_buffer *)data;
- 
+
    mem->data = (char *)realloc(mem->data, mem->size + realsize + 1);
    if (mem->data) {
      memcpy(&(mem->data[mem->size]), ptr, realsize);
@@ -133,7 +144,7 @@ static size_t write_web_buf(void* ptr, size_t size, size_t nmemb, void *data)
 #endif
 double radian(double d)
 {
-    return d * PI / 180.0;  
+    return d * PI / 180.0;
 }
 
 double get_distance(double lat1, double lng1, double lat2, double lng2)
@@ -142,9 +153,9 @@ double get_distance(double lat1, double lng1, double lat2, double lng2)
     double radLat2 = radian(lat2);
     double a = radLat1 - radLat2;
     double b = radian(lng1) - radian(lng2);
-    
+
     double dst = 2 * asin((sqrt(pow(sin(a / 2), 2) + cos(radLat1) * cos(radLat2) * pow(sin(b / 2), 2) )));
-    
+
     dst = dst * EARTH_RADIUS;
     dst= round(dst * 10000) / 10000;
     return dst;
@@ -155,10 +166,10 @@ static void XMLCALL start_element(void *userData, const char *el, const char **a
     int i;
 
     if (depth == 1 && strcmp(el, "client") == 0) {
-      
+
         struct client_info *p_client = (struct client_info *)userData;
 
-        for (i = 0; atts[i]; i += 2) { 
+        for (i = 0; atts[i]; i += 2) {
             //printf(" %s \n", atts[i]);
             if (strcmp(atts[i], "ip") == 0)
               strcpy(p_client->ip, atts[i + 1]);
@@ -176,7 +187,7 @@ static void XMLCALL start_element(void *userData, const char *el, const char **a
     if (depth == 2 && strcmp(el, "server") == 0) {
         struct server_info *p_server = (struct server_info *)userData;
 
-        for (i = 0; atts[i]; i += 2) { 
+        for (i = 0; atts[i]; i += 2) {
             //printf(" %s \n", atts[i]);
             if (strcmp(atts[i], "url") == 0)
                 strcpy(p_server->url, atts[i + 1]);
@@ -204,9 +215,9 @@ static void XMLCALL end_element(void *userData, const char *name)
         p_server->distance = get_distance(client.lat, client.lon, p_server->lat, p_server->lon);
 
         for (i = 0; i < MAX_CLOSEST_SERVER_NUM; i++) {
-            
+
             if ( servers[i].url[0] == 0 ) {
-                
+
                 break;
             }
         }
@@ -214,8 +225,8 @@ static void XMLCALL end_element(void *userData, const char *name)
         if ( i == MAX_CLOSEST_SERVER_NUM ) {
 
             for (i = 0; i <MAX_CLOSEST_SERVER_NUM; i++) {
-            
-                if (servers[i].distance >  p_server->distance) { 
+
+                if (servers[i].distance >  p_server->distance) {
                     break;
                 }
             }
@@ -233,13 +244,13 @@ static int do_latency(char *p_url)
     CURL *curl;
     CURLcode res;
     long response_code;
-    
+
     curl = curl_easy_init();
 
     sprintf(latency_url, "%s%s", p_url, LATENCY_TXT_URL);
-    
     curl_easy_setopt(curl, CURLOPT_URL, latency_url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
     curl_easy_setopt(curl, CURLOPT_TIMEOUT, 3L);
     res = curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
@@ -247,7 +258,7 @@ static int do_latency(char *p_url)
 
     if (res != CURLE_OK || response_code != 200) {
 
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); 
+        //printf("curl_easy_perform() failed: %s %ld\n", curl_easy_strerror(res), response_code);
         return NOK;
     }
     return OK;
@@ -274,18 +285,18 @@ static void* do_download(void* data)
     struct thread_para* p_para = (struct thread_para*)data;
     double length = 0;
     double time = 0, time1 = 0, time2;
-    
+
     curl = curl_easy_init();
-    
+
     //printf("image url = %s\n", p_para->url);
     curl_easy_setopt(curl, CURLOPT_URL, p_para->url);
-    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, p_para);
     res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); 
+        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         curl_easy_cleanup(curl);
-        return;
+        return NULL;
     }
     curl_easy_getinfo(curl, CURLINFO_SIZE_DOWNLOAD, &length);
     curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &time);
@@ -294,20 +305,124 @@ static void* do_download(void* data)
     //printf("Length is %lf %lf %lf %lf\n", length, time, time1, time2);
     p_para->result = length;
     curl_easy_cleanup(curl);
+    return NULL;
+}
+static void loop_threads(struct thread_para *p_thread, int num_thread, double *speed, int *p_num_speed)
+{
+    char alive = 0;
+    int  num = 0, num_speed = 0;
+
+
+    do {
+        int i, time = 0;
+        double sum = 0;
+        alive = 0;
+
+        for (i = 0;i < num_thread; i++) {
+            if (p_thread[i].result == 0)
+                alive = 1;
+            pthread_mutex_lock(&p_thread->lock);
+            sum += p_thread[i].now;
+            p_thread[i].now = 0;
+            pthread_mutex_unlock(&p_thread->lock);
+            //printf("p_thread[i].now = %lf\n", p_thread[i].now);
+        }
+
+        usleep(RECORED_EVERY_SEC*1000*1000); //0.2s
+        if (sum == 0 && num_speed == 0)
+            continue;
+
+        if (num_speed < *p_num_speed) {
+
+            speed[num_speed] = sum/RECORED_EVERY_SEC; //Byte
+            num_speed++;
+        }
+
+        if (++num % PRINT_RECORED_NUM == 0) {
+
+             printf(".");
+             fflush(stdout);
+        }
+
+    }while(alive);
+    printf("\n");
+    *p_num_speed = num_speed;
     return;
 }
 
+static double calculate_average_speed(double *p_speed, int num_speed)
+{
+
+    int i = 0;
+    int start ,end;
+    double sum = 0;
+    for (i = 0; i < num_speed; i++) {
+
+        int j, min;
+
+        for ( min = i, j =  i + 1; j < num_speed; j++) {
+
+            if (p_speed[min] > p_speed[j])
+                min = j;
+        }
+        if (min != i) {
+
+            double          tmp;
+            tmp           = p_speed[i];
+            p_speed[i]    = p_speed[min];
+            p_speed[min]    = tmp;
+        }
+
+    }
+#if 0
+    for (i = 0; i < num_speed; i++) {
+        printf("%0.2lf ", p_speed[i]*8/(1024*1024));
+        if (i%10 == 0)
+            printf("\n");
+    }
+#endif
+    /*The fastest 10% and slowest 20% of the slices are discarded*/
+    start = num_speed*0.2;
+    end   = num_speed - (int)(num_speed*0.1);
+    //end = num_speed;
+    for (i = start; i < end; i++) {
+
+        sum += p_speed[i];
+    }
+    //printf("speed = %0.2lf\n", (sum*8/(end - start))/(1024*1024));
+
+    return sum/(end - start);
+}
+
+static int init_instant_speed(double **p_speed, int *p_speed_num)
+{
+    *p_speed_num    = SPEEDTEST_TIME_MAX/RECORED_EVERY_SEC + 1;
+    *p_speed         = (double*)malloc((*p_speed_num)*sizeof(double));
+
+    if (*p_speed == NULL) {
+
+        fprintf(stderr, "malloc failed\n");
+        return -1;
+    }
+    memset(*p_speed, 0, (*p_speed_num)*sizeof(double));
+    return 0;
+}
 static int test_download(char *p_url, int num_thread, int dsize, char init)
 {
-    struct timeval s_time, e_time;
+    struct timeval s_time;
     int time, i, error;
     struct thread_para paras[THREAD_NUM_MAX];
     double sum = 0;
+    double speed = 0;
+    double *instant_speed = NULL;
+    int    speed_num = 0;
 
     gettimeofday(&s_time, NULL);
+    init_instant_speed(&instant_speed, &speed_num);
 
     for ( i = 0; i < num_thread; i++) {
 
+        memset(&paras[i], 0, sizeof(struct thread_para));
         sprintf(paras[i].url, "%s/speedtest/random%dx%d.jpg", p_url, dsize, dsize);
         paras[i].result = 0;
         error = pthread_create(&paras[i].tid, NULL, do_download, (void*)&paras[i]);
@@ -315,31 +430,31 @@ static int test_download(char *p_url, int num_thread, int dsize, char init)
         if ( error != 0)
             printf("Can't Run thread num %d, error %d\n", i, error);
     }
+
     if (init != 0)
     {
-        char running = 0;
-
-        do {
-            usleep(400*1000);
-            running = 0;
-            for (i = 0;i < num_thread; i++) {
-                if (paras[i].result == 0)
-                    running = 1;
-            }
-            fprintf(stderr, ".");
-        }while(running);
-       fprintf(stderr, "\n");
+        loop_threads(paras, num_thread, instant_speed, &speed_num);
     }
+
     for (i = 0;i < num_thread; i++) {
         pthread_join(paras[i].tid, NULL);
         sum += paras[i].result;
     }
+
+    if (init != 0) {
+
+        speed = calculate_average_speed(instant_speed, speed_num);
+    }else {
+
+        struct timeval  e_time;
+
     gettimeofday(&e_time, NULL);
-
     time = calc_past_time(&s_time, &e_time);
-
+        speed = (sum*1000)/time;
     //printf("msec = %d speed = %0.2fMbps\n", time, ((sum*8*1000/time))/(1024*1024));
-    return (sum*1000)/time;
+    }
+    free(instant_speed);
+    return speed;
 }
 
 static size_t read_data(void* ptr, size_t size, size_t nmemb, void *userp)
@@ -361,12 +476,12 @@ static size_t read_data(void* ptr, size_t size, size_t nmemb, void *userp)
             if (para->data[i - 1] + 1 > 'Z')
                 para->data[i - 1] = 'A';
             para->data[i] = para->data[i - 1] + 1;
-         
+
         }
     }
 #endif
     if (para->upload_size > size * nmemb) {
-        
+
         length = size * nmemb < 16284 ? size*nmemb : 16284;
     }
     else
@@ -375,6 +490,9 @@ static size_t read_data(void* ptr, size_t size, size_t nmemb, void *userp)
 
     para->upload_size -= length;
 
+    pthread_mutex_lock(&para->lock);
+    para->now += length;
+    pthread_mutex_unlock(&para->lock);
     return  length;
 }
 
@@ -387,41 +505,50 @@ static int do_upload(struct thread_para* para)
     double size_upload;
 
     curl = curl_easy_init();
-    
+
     curl_easy_setopt(curl, CURLOPT_URL, para->url);
     curl_easy_setopt(curl, CURLOPT_POST, 1L);
     curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_data);
     curl_easy_setopt(curl, CURLOPT_READDATA, para);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, NULL);
     curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE , (curl_off_t)para->upload_size);
     res = curl_easy_perform(curl);
     if (res != CURLE_OK) {
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); 
+        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         curl_easy_cleanup(curl);
         return NOK;
     }
     curl_easy_getinfo(curl, CURLINFO_SIZE_UPLOAD, &size_upload);
-    para->result = size_upload;
+    if (size_upload == 0) { 
+        // for 413 Request Entity Too Large
+        printf("This server do not support large request");
+        para->result = 1;
+    } else
+        para->result = size_upload;
     //printf("size upload = %lf\n", size_upload);
     curl_easy_cleanup(curl);
     return OK;
 }
 
-static int test_upload(char *p_url, int num_thread, long speed, char *p_ext, char init)
+static int test_upload(char *p_url, int num_thread, long size, char *p_ext, char init)
 {
-    struct timeval s_time, e_time;
-    int time;
+    struct timeval s_time;
     int i, error;
     struct thread_para paras[THREAD_NUM_MAX];
-    double sum = 0;
-
+    double sum = 0, speed = 0;
+    double *instant_speed = NULL;
+    int    speed_num = 0;
     gettimeofday(&s_time, NULL);
 
+    init_instant_speed(&instant_speed, &speed_num);
+
     for ( i = 0; i < num_thread; i++) {
+        memset(&paras[i], 0, sizeof(struct thread_para));
         sprintf(paras[i].url, "%s/speedtest/upload.%s", p_url, p_ext);
         paras[i].result = 0;
-        paras[i].upload_size = speed/num_thread;
+        paras[i].upload_size = size/num_thread;
         //printf("szeleft = %ld\n", paras[i].upload_size);
         error = pthread_create(&paras[i].tid, NULL, (void*)do_upload, (void*)&paras[i]);
 
@@ -430,29 +557,26 @@ static int test_upload(char *p_url, int num_thread, long speed, char *p_ext, cha
     }
     if (init != 0)
     {
-        char running = 0;
-
-        do {
-            usleep(400*1000);
-            running = 0;
-            for (i = 0;i < num_thread; i++) {
-                if (paras[i].result == 0)
-                    running = 1;
-            }
-            fprintf(stderr, ".");
-        }while(running);
-        fprintf(stderr, "\n");
+        loop_threads(paras, num_thread, instant_speed, &speed_num);
     }
     for (i = 0;i < num_thread; i++) {
         pthread_join(paras[i].tid, NULL);
         sum += paras[i].result;
     }
+    if (init != 0) {
+
+        speed = calculate_average_speed(instant_speed, speed_num);
+    }else {
+        struct timeval e_time;
+        int time;
+
     gettimeofday(&e_time, NULL);
-
     time = calc_past_time(&s_time, &e_time);
-
+        speed = (sum*1000)/time; //bytes per second
+    }
     //printf("msec = %d speed = %0.2fMbps\n", time, ((sum*8*1000/time))/(1024*1024));
-    return (sum*1000)/time;
+    free(instant_speed);
+    return speed;
 }
 
 static int get_download_filename(double speed, int num_thread)
@@ -486,7 +610,7 @@ static int get_upload_extension(char *server, char *p_ext)
     int i;
 
     memset(&web, 0, sizeof(web));
-    
+
     curl = curl_easy_init();
 
     curl_easy_setopt(curl, CURLOPT_URL, server);
@@ -496,11 +620,11 @@ static int get_upload_extension(char *server, char *p_ext)
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
     if (res != CURLE_OK) {
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); 
+        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         return NOK;
     }
     p = strstr(web.data, UPLOAD_EXTENSION_TAG);
-    if (p == NULL || 
+    if (p == NULL ||
         sscanf(p + strlen(UPLOAD_EXTENSION_TAG), "%*[^a-zA-Z]%[a-zA-Z]", p_ext) <= 0) {
         fprintf(stderr, "Upload extension not found\n");
         return NOK;
@@ -530,7 +654,7 @@ static int get_client_info(struct client_info *p_client)
     curl_easy_cleanup(curl);
     if (res != CURLE_OK) {
 
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); 
+        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         return NOK;
     }
     XML_SetUserData(xml, p_client);
@@ -541,7 +665,7 @@ static int get_client_info(struct client_info *p_client)
         exit(-1);
     }
     free(web.data);
-    
+
     return OK;
 }
 
@@ -567,7 +691,7 @@ static int get_closest_server()
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
     if (res != CURLE_OK) {
-        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); 
+        printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         return NOK;
     }
     XML_SetUserData(xml, &server);
@@ -600,7 +724,7 @@ static int get_best_server(int *p_index)
             *p_index = i;
         }
     }
-    if (minimum == DBL_MAX) 
+    if (minimum == DBL_MAX)
         return NOK;
     return OK;
 }
@@ -611,7 +735,7 @@ int main(int argc, char *argv[])
     char    server_url[URL_LENGTH_MAX], ext[UPLOAD_EXT_LENGTH_MAX];
     double  latency, speed, download_speed, upload_speed;
     int     dsize, sindex;
-    
+
     sindex      = -1;
     num_thread  = 4;
     dsize       = INIT_DOWNLOAD_FILE_RESOLUTION;
@@ -656,16 +780,16 @@ int main(int argc, char *argv[])
     curl_global_init(CURL_GLOBAL_ALL);
 
     latency = test_latency(server_url);
-    if (latency == DBL_MAX) 
+    if (latency == DBL_MAX)
         exit(-1);
     printf("Server latency is %0.0fms\n", latency);
-    
+
     speed = test_download(server_url, num_thread, dsize, 0);
-    
+
     dsize = get_download_filename(speed, num_thread);
     fprintf(stderr, "Testing download speed");
     download_speed = test_download(server_url, num_thread, dsize, 1);
-    
+
     printf("Download speed: %0.2fMbps\n", ((download_speed*8)/(1024*1024)));
 
     if (ext[0] == 0 && get_upload_extension(server_url, ext) != OK)
@@ -677,8 +801,5 @@ int main(int argc, char *argv[])
     upload_speed = test_upload(server_url, num_thread, speed*SPEEDTEST_TIME_MAX, ext, 1);
 
     printf("Upload speed: %0.2fMbps\n", ((upload_speed*8)/(1024*1024)));
-    
+
 }
-
-
-
